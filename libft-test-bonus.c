2b1094fac2faeb31567f371ff4fb02c430339dbd
Salah Adawi <sadawi@student.hive.fi>
2019-10-30 14:15:50 +0200

    split bonus to different file

---
+++ libft-test-bonus.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sadawi <sadawi@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/29 14:27:03 by sadawi            #+#    #+#             */
+/*   Updated: 2019/10/30 14:14:16 by sadawi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void	del(void *content, size_t content_size)
+{
+	free(content);
+	(void)(content_size);
+}
+
+void    fill(char *s, char c)
+{
+    int i;
+
+    i = 0;
+    while (s[i])
+    {
+        s[i] = c;
+        i++;
+    }
+}
+
+void	changelist(t_list *elem)
+{
+	elem->content = strdup("123456789");
+	elem->content_size = 9;
+}
+
+t_list *listmap(t_list *elem)
+{
+	t_list *tmp;
+	tmp = ft_lstnew(elem->content, elem->content_size);
+	fill(tmp->content, 'b');
+	return (tmp);
+}
+
+int	bonus(void)
+{
+	t_list *link;
+	t_list *link2;
+	t_list *link3;
+
+		printf("          PART 3 FUNCTIONS\n");
+// lstnew
+		link = ft_lstnew("Hello World!", 13);
+		printf("ft_lstnew: %d ", strcmp(link->content, "Hello World!")); //1
+		printf("%d ", !(link->content_size == 13)); //2
+		printf("%d\n", !(link->next == NULL)); //3
+// lstadd
+		link2 = ft_lstnew("test123", 8);
+		ft_lstadd(&link, link2);
+		printf("ft_lstadd: %d ", strcmp(link->content, "test123")); //1
+		printf("%d ", !(link->content_size == 8)); //2
+		link = link->next;
+		printf("%d ", strcmp(link->content, "Hello World!")); //3
+		printf("%d ", !(link->content_size == 13)); //4
+		printf("%d\n", !(link->next == NULL)); //5
+// lstiter
+		ft_lstadd(&link, link2);
+		ft_lstiter(link, changelist);
+		printf("ft_lstiter: %d ", strcmp(link->content, "123456789")); //1
+		printf("%d ", !(link->content_size == 9)); //2
+		link = link->next;
+		printf("%d ", strcmp(link->content, "123456789")); //3
+		printf("%d\n", !(link->content_size == 9)); //4
+// lstmap
+		ft_lstadd(&link, link2);
+		link3 = ft_lstmap(link, listmap);
+		printf("ft_lstmap: %d ", strcmp(link3->content, "bbbbbbbbb")); //1
+		printf("%d ", !(link3->content_size == 9)); //2
+		link3 = link3->next;
+		printf("%d ", strcmp(link3->content, "bbbbbbbbb")); //3
+		printf("%d ", !(link3->content_size == 9)); //4
+		printf("%d ", strcmp(link->content, "123456789")); //5
+		printf("%d ", !(link->content_size == 9)); //6
+		link = link->next;
+		printf("%d ", strcmp(link->content, "123456789")); //7
+		printf("%d\n", !(link->content_size == 9)); //8
+// lstdelone
+		ft_lstadd(&link, link2);
+		ft_lstadd(&link, link3);
+		link3 = link->next;
+		ft_lstdelone(&link, del);
+		printf("ft_lstdelone: %d ", !(link == NULL)); //1
+		printf("%d\n", strcmp(link3->content, "123456789")); //2
+// lstdel
+		link = link3->next;
+		ft_lstdel(&link3, del);
+		printf("ft_lstdel: %d ", !(link3 == NULL)); //1
+		printf("%d\n", (ft_strequ(link->content, "123456789"))); //2
+
+		return (0);
+}
--- libft-test.c
+++ libft-test.c
@@ -6,7 +6,7 @@
 /*   By: sadawi <sadawi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/29 14:27:03 by sadawi            #+#    #+#             */
-/*   Updated: 2019/10/30 13:31:13 by sadawi           ###   ########.fr       */
+/*   Updated: 2019/10/30 14:14:37 by sadawi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-void	del(void *content, size_t content_size)
-{
-	free(content);
-	(void)(content_size);
-}
-
-void    fill(char *s, char c)
-{
-    int i;
-
-    i = 0;
-    while (s[i])
-    {
-        s[i] = c;
-        i++;
-    }
-}
+int	bonus(void);
 
 void clear(char *s1, char *s2)
 {
@@ -66,20 +50,6 @@ char changemapi(unsigned int i, char c)
 	return ('b');
 }
 
-void	changelist(t_list *elem)
-{
-	elem->content = strdup("123456789");
-	elem->content_size = 9;
-}
-
-t_list *listmap(t_list *elem)
-{
-	t_list *tmp;
-	tmp = ft_lstnew(elem->content, elem->content_size);
-	fill(tmp->content, 'b');
-	return (tmp);
-}
-
 int	main(int argc, char **argv)
 {
 	char s1[20] = "Hello World!";
@@ -87,9 +57,6 @@ int	main(int argc, char **argv)
 	char *p1;
 	char *p2;
 	char **p3;
-	t_list *link;
-	t_list *link2;
-	t_list *link3;
 
 	if (argc == 1 || !strcmp(argv[1], "1"))
 	{
@@ -385,56 +352,6 @@ int	main(int argc, char **argv)
 		printf("%d\n", strcmp(p1, "-2147483648")); //4
 	}
 	if (argc == 1 || !strcmp(argv[1], "3"))
-	{
-		printf("          PART 3 FUNCTIONS\n");
-// lstnew
-		link = ft_lstnew("Hello World!", 13);
-		printf("ft_lstnew: %d ", strcmp(link->content, "Hello World!")); //1
-		printf("%d ", !(link->content_size == 13)); //2
-		printf("%d\n", !(link->next == NULL)); //3
-// lstadd
-		link2 = ft_lstnew("test123", 8);
-		ft_lstadd(&link, link2);
-		printf("ft_lstadd: %d ", strcmp(link->content, "test123")); //1
-		printf("%d ", !(link->content_size == 8)); //2
-		link = link->next;
-		printf("%d ", strcmp(link->content, "Hello World!")); //3
-		printf("%d ", !(link->content_size == 13)); //4
-		printf("%d\n", !(link->next == NULL)); //5
-// lstiter
-		ft_lstadd(&link, link2);
-		ft_lstiter(link, changelist);
-		printf("ft_lstiter: %d ", strcmp(link->content, "123456789")); //1
-		printf("%d ", !(link->content_size == 9)); //2
-		link = link->next;
-		printf("%d ", strcmp(link->content, "123456789")); //3
-		printf("%d\n", !(link->content_size == 9)); //4
-// lstmap
-		ft_lstadd(&link, link2);
-		link3 = ft_lstmap(link, listmap);
-		printf("ft_lstmap: %d ", strcmp(link3->content, "bbbbbbbbb")); //1
-		printf("%d ", !(link3->content_size == 9)); //2
-		link3 = link3->next;
-		printf("%d ", strcmp(link3->content, "bbbbbbbbb")); //3
-		printf("%d ", !(link3->content_size == 9)); //4
-		printf("%d ", strcmp(link->content, "123456789")); //5
-		printf("%d ", !(link->content_size == 9)); //6
-		link = link->next;
-		printf("%d ", strcmp(link->content, "123456789")); //7
-		printf("%d\n", !(link->content_size == 9)); //8
-// lstdelone
-		ft_lstadd(&link, link2);
-		ft_lstadd(&link, link3);
-		link3 = link->next;
-		ft_lstdelone(&link, del);
-		printf("ft_lstdelone: %d ", !(link == NULL)); //1
-		printf("%d\n", strcmp(link3->content, "123456789")); //2
-// lstdel
-		link = link3->next;
-		ft_lstdel(&link3, del);
-		printf("ft_lstdel: %d ", !(link3 == NULL)); //1
-		printf("%d\n", (ft_strequ(link->content, "123456789"))); //2
-
-}
+		bonus();
 		return (0);
 }
